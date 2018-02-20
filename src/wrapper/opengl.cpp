24f732286b0c77ce5d2233940aa94d0b25959989
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-02-19 19:03:29 -0500

    Wrapped shader related resources

--- src/wrapper/opengl.cpp
+++ src/wrapper/opengl.cpp
@@ -10,6 +10,17 @@ vertex_array::vertex_array(GLuint vao) noexcept
 
 }
 
+vertex_array::vertex_array(vertex_array&& other) noexcept
+: vao{other.vao} {
+
+}
+
+vertex_array& vertex_array::operator=(vertex_array&& other) noexcept {
+    std::swap(vao, other.vao);
+
+    return *this;
+}
+
 vertex_array::~vertex_array() noexcept {
     glDeleteVertexArrays(1, &vao);
 }
@@ -43,6 +54,16 @@ buffer::buffer(GLuint vbo) noexcept
 
 }
 
+buffer::buffer(buffer&& other) noexcept
+: vbo{other.vbo} {
+    other.vbo = 0;
+}
+
+buffer& buffer::operator=(buffer&& other) noexcept {
+    std::swap(vbo, other.vbo);
+    return *this;
+}
+
 buffer::~buffer() noexcept {
     glDeleteBuffers(1, &vbo);
 }
@@ -65,4 +86,140 @@ buffer buffer::make() noexcept {
 
     return buffer(vbo);
 }
+
+//======================================================================================================================
+// SHADER
+//======================================================================================================================
+shader::shader(GLenum type) noexcept
+: raw(glCreateShader(type)){
+
+}
+
+shader::shader(shader&& other) noexcept
+: raw(other.raw){
+    other.raw = 0;
+}
+
+shader& shader::operator=(shader&& other) noexcept {
+    std::swap(raw, other.raw);
+
+    return *this;
+}
+
+shader::~shader() noexcept {
+    glDeleteShader(raw);
+}
+
+shader::compilation_status shader::compile(std::istream& source_stream) const noexcept {
+    std::string source;
+    char letter;
+    while(source_stream.get(letter)) {
+        source.push_back(letter);
+    }
+
+    return compile(source);
+}
+
+shader::compilation_status shader::compile(const char* source) const noexcept {
+    bool compiled = false;
+
+    // Load the sources into the shader
+    glShaderSource(raw, 1, &source, nullptr);
+
+    // Compile the shader
+    glCompileShader(raw);
+    GLint is_compiled;
+    glGetShaderiv(raw, GL_COMPILE_STATUS, &is_compiled);
+
+    GLsizei log_len = 0;
+    glGetShaderiv(raw, GL_INFO_LOG_LENGTH, &log_len);
+
+    std::string message;
+    GLchar* message_buffer = new(std::nothrow) GLchar[log_len];
+    if(message_buffer) {
+        glGetShaderInfoLog(raw, log_len, nullptr, message_buffer);
+        message = std::string{message_buffer};
+        delete[] message_buffer;
+    }
+
+    return compilation_status{is_compiled == GL_TRUE, message};
+}
+
+shader::compilation_status shader::compile(const std::string& source) const noexcept {
+    return compile(source.c_str());
+}
+
+shader::operator GLuint() const noexcept {
+    return raw;
+}
+
+//======================================================================================================================
+// PROGRAM
+//======================================================================================================================
+program::link_status::link_status(bool is_linked, const std::string& msg)
+: is_linked{is_linked}
+, msg{msg} {
+
+}
+
+bool program::link_status::good() const noexcept {
+    return is_linked;
+}
+
+program::link_status::operator bool() const noexcept {
+    return good();
+}
+
+const std::string& program::link_status::message() const noexcept {
+    return msg;
+}
+
+program::program() noexcept
+: prog{glCreateProgram()}{
+
+}
+
+program::program(program&& other) noexcept
+: prog{other.prog} {
+    other.prog = 0;
+}
+
+program& program::operator=(program&& other) noexcept {
+    std::swap(prog, other.prog);
+
+    return *this;
+}
+
+program::~program() noexcept {
+    glDeleteProgram(prog);
+}
+
+program::operator GLuint() const noexcept {
+    return prog;
+}
+
+program::link_status program::link() const noexcept {
+    glLinkProgram(prog);
+
+    GLint status;
+    glGetProgramiv(prog, GL_LINK_STATUS, &status);
+
+    GLsizei log_len = 0;
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &log_len);
+
+    std::string message;
+    GLchar* message_buffer = new(std::nothrow) GLchar[log_len];
+    if(message_buffer) {
+        glGetProgramInfoLog(prog, log_len, nullptr, message_buffer);
+        message = std::string{message_buffer};
+        delete[] message_buffer;
+    }
+
+    return link_status{status == GL_TRUE, ""};
+}
+
+void program::bind() const noexcept {
+    glUseProgram(prog);
+}
+
 }
--- src/wrapper/opengl.hpp
+++ src/wrapper/opengl.hpp
@@ -1,6 +1,8 @@
 #ifndef MMAP_DEMO_OPENGL_HPP
 #define MMAP_DEMO_OPENGL_HPP
 
+#include <iostream>
+#include <string>
 #include <GL/gl3w.h>
 #include <SDL_opengl.h>
 
@@ -12,6 +14,156 @@ struct is_bindable {
     static const bool value = false;
 };
 
+struct vertex_shader_type {
+    static const GLenum value = GL_VERTEX_SHADER;
+};
+struct fragment_shader_type {
+    static const GLenum value = GL_FRAGMENT_SHADER;
+};
+
+template<typename T>
+struct is_shader_type {
+    static const bool value = false;
+};
+
+template<>
+struct is_shader_type<vertex_shader_type> {
+    static const bool value = true;
+};
+
+template<>
+struct is_shader_type<fragment_shader_type> {
+    static const bool value = true;
+};
+
+class shader {
+    GLuint raw;
+protected:
+    shader(GLenum type) noexcept;
+public:
+    class compilation_status {
+        std::string msg;
+        bool has_failed;
+    public:
+        compilation_status(bool has_failed, const std::string& msg)
+        : msg{msg}
+        , has_failed{has_failed} {
+
+        }
+
+        bool good() const noexcept {
+            return !has_failed;
+        }
+
+        operator bool() const noexcept {
+            return good();
+        }
+
+        const std::string& message() const noexcept {
+            return msg;
+        }
+    };
+
+    // Copy is disabled
+    shader(const shader&) = delete;
+    shader& operator=(const shader&) = delete;
+
+    // Move is enabled
+    shader(shader&&) noexcept;
+    shader& operator=(shader&&) noexcept;
+
+    virtual ~shader() noexcept;
+
+    compilation_status compile(const char* source) const noexcept;
+    compilation_status compile(const std::string& source) const noexcept;
+    compilation_status compile(std::istream& source_stream) const noexcept;
+
+    operator GLuint() const noexcept;
+};
+
+template<typename TYPE>
+class typed_shader : public shader {
+    static_assert(is_shader_type<TYPE>::value, "you must specify a valid shader type");
+public:
+    using type = TYPE;
+
+    typed_shader() noexcept
+    : shader(TYPE::value) {
+
+    }
+};
+
+using vertex_shader = typed_shader<vertex_shader_type>;
+using fragment_shader = typed_shader<fragment_shader_type>;
+
+template<typename T>
+struct is_shader {
+    static const bool value = false;
+};
+
+template<>
+struct is_shader<shader> {
+    static const bool value = true;
+};
+
+template<typename T>
+struct is_shader<typed_shader<T>> {
+    static const bool value = true;
+};
+
+class program {
+    GLuint prog;
+public:
+    class link_status {
+        bool is_linked;
+        std::string msg;
+    public:
+        link_status(bool is_linked, const std::string& msg);
+
+        bool good() const noexcept;
+        operator bool() const noexcept;
+
+        const std::string& message() const noexcept;
+    };
+
+    program() noexcept;
+
+    program(const program&) = delete;
+    program& operator=(const program&) = delete;
+
+    program(program&&) noexcept;
+    program& operator=(program&&) noexcept;
+
+    ~program() noexcept;
+
+    template<typename SHADER>
+    void attach(const SHADER& shader) const noexcept {
+        static_assert(is_shader<SHADER>::value, "you must specify a shader type");
+
+        glAttachShader(prog, shader);
+    }
+
+    template<typename It>
+    void attach(It begin, It end) const noexcept {
+        static_assert(is_shader<typename It::value_type>::value, "you must specify an iterator on shader");
+
+        std::for_each(begin, end, [this](const typename It::value_type& shader){
+            attach(shader);
+        });
+    }
+
+    operator GLuint() const noexcept;
+
+    link_status link() const noexcept;
+
+    void bind() const noexcept;
+};
+
+template<>
+struct is_bindable<program> {
+    static const bool value = true;
+};
+
 class vertex_array {
     GLuint vao {};
 
@@ -21,8 +173,8 @@ public:
     vertex_array() noexcept = default;
 
     // A vertex array can be moved
-    vertex_array(vertex_array&&) noexcept = default;
-    vertex_array& operator=(vertex_array&&) noexcept = default;
+    vertex_array(vertex_array&&) noexcept;
+    vertex_array& operator=(vertex_array&&) noexcept;
 
     // A vertex array cannot be copied
     vertex_array(const vertex_array&) = delete;
@@ -54,8 +206,8 @@ public:
     buffer(const buffer&) = delete;
     buffer& operator=(const buffer&) = delete;
 
-    buffer(buffer&&) noexcept = default;
-    buffer& operator=(buffer&&) noexcept = default;
+    buffer(buffer&&) noexcept;
+    buffer& operator=(buffer&&) noexcept;
 
     ~buffer() noexcept;
 
