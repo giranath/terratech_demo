a94c301eaa1b49a3b27c6d8cca1bdaf011b53210
DESKTOP-3OFBCS1\sharp <sharpjuices@gmail.com>
2018-03-07 12:06:50 -0500

    Ajout des input++ #67

--- src/control/input_handler.hpp
+++ src/control/input_handler.hpp
@@ -8,45 +8,110 @@
 
 #include <memory>
 #include <map>
-
+#include <vector>
+#include <algorithm>
+#include <tuple>
 class input_handler
 {
-
+    std::map<int, bool> key_pressed;
+    std::map<int, std::vector<std::pair<std::unique_ptr<command>, bool>*>> key_to_command;
     camera* cam;
-    std::map<int, command*> binding;
+    sdl::context<>* sdl;
+    std::pair<std::unique_ptr<command>, bool> ctrl_m;
+    std::pair<std::unique_ptr<command>, bool> arrow_left;
+    std::pair<std::unique_ptr<command>, bool> arrow_right;
+    std::pair<std::unique_ptr<command>, bool> arrow_up;
+    std::pair<std::unique_ptr<command>, bool> arrow_down;
 
-    std::unique_ptr<look_left_command> look_left;
-    std::unique_ptr<look_right_command> look_right;
-    std::unique_ptr<look_up_command> look_up;
-    std::unique_ptr<look_down_command> look_down;
-    std::unique_ptr<wireframe_command> wireframe;
-
-public:
-    void bind_key_to_command(const int& key, command* command)
+    void pressed(const int& key)
     {
-        binding[key] = command;
-    }
-    input_handler(camera& cam) : cam{ &cam },
-        look_left(std::make_unique<look_left_command>(&cam)),
-        look_right(std::make_unique<look_right_command>(&cam)),
-        look_up(std::make_unique<look_up_command>(&cam)),
-        look_down(std::make_unique<look_down_command>(&cam)),
-        wireframe(std::make_unique<wireframe_command>())
-
-    {
-        bind_key_to_command(SDLK_LEFT, look_left.get());
-        bind_key_to_command(SDLK_RIGHT, look_right.get());
-        bind_key_to_command(SDLK_UP, look_up.get());
-        bind_key_to_command(SDLK_DOWN, look_down.get());
-        bind_key_to_command(SDLK_m, wireframe.get());
+        key_pressed[key] = true;
     }
 
-    void is_pressed(const int& key)
+    void released(const int& key)
     {
-        if (binding[key])
+        key_pressed[key] = false;
+        for (auto& v : key_to_command[key])
         {
-            binding[key]->execute();
+            v->second = true;
         }
     }
+
+    void current_input()
+    {
+        for (auto event : sdl->poll_events()) {
+            if (event.type == SDL_KEYDOWN)
+            {
+                pressed(event.key.keysym.sym);
+            }
+            else if (event.type == SDL_KEYUP)
+            {
+                released(event.key.keysym.sym);
+            }
+        }
+    }
+
+public:
+    input_handler(camera& cam, sdl::context<>& sdl) : 
+        cam{ &cam },
+        sdl{&sdl},
+        arrow_left(std::make_unique<look_left_command>(&cam), true),
+        arrow_right(std::make_unique<look_right_command>(&cam), true),
+        arrow_up(std::make_unique<look_up_command>(&cam), true),
+        arrow_down(std::make_unique<look_down_command>(&cam), true),
+        ctrl_m(std::make_unique<wireframe_command>(), true)
+
+    {
+        key_to_command[SDLK_m].push_back(&ctrl_m);
+        key_to_command[SDLK_LCTRL].push_back(&ctrl_m);
+
+        key_to_command[SDLK_LEFT].push_back(&arrow_left);
+        key_to_command[SDLK_RIGHT].push_back(&arrow_right);
+        key_to_command[SDLK_UP].push_back(&arrow_up);
+        key_to_command[SDLK_DOWN].push_back(&arrow_down);
+
+    }
+
+    
+    void handle_input()
+    {
+        current_input();
+
+        if (key_pressed[SDLK_LCTRL] && key_pressed[SDLK_m])
+        {
+            if (ctrl_m.second)
+            {
+                ctrl_m.first->execute();
+                if (!ctrl_m.first->is_repeatable())
+                {
+                    ctrl_m.second = false;
+                }
+            }
+        }
+        else if (key_pressed[SDLK_m])
+        {
+            int i = 0;
+        }
+
+
+        if (key_pressed[SDLK_LEFT])
+        {
+            arrow_left.first->execute();
+        }
+
+        if (key_pressed[SDLK_RIGHT])
+        {
+            arrow_right.first->execute();
+        }
+        if (key_pressed[SDLK_UP])
+        {
+            arrow_up.first->execute();
+        }
+        if (key_pressed[SDLK_DOWN])
+        {
+            arrow_down.first->execute();
+        }
+    }
+
 };
 #endif
