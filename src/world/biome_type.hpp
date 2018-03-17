a8b5b4b32ad3bc9c14d04fc019fe71328fe9ad83
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-03-17 12:29:04 -0400

    Fixed build

---
+++ src/world/biome_type.hpp
@@ -0,0 +1,13 @@
+#ifndef MMAP_DEMO_BIOME_TYPE_HPP
+#define MMAP_DEMO_BIOME_TYPE_HPP
+
+enum class biome_type {
+    grass,
+    rock,
+    snow,
+    desert,
+    water,
+    unknown
+};
+
+#endif //MMAP_DEMO_BIOME_TYPE_HPP
