c2345d4cc1541debc73ead8d6bf6a0b565e95442
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-03-18 18:00:33 -0400

    Remove tabs

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,3 +1,7 @@
 int main(int argc, const char** argv) {
-
+    // 1. The server generates a map
+    // 2. The server waits for two connections
+    // 3. The game starts
+    // 4. The server receives the actions from both player
+    // 5. The server regularly send current game state to both player
 }
