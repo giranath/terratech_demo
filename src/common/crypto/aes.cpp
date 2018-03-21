6f4fcc294d32ee81d700240dcf441357fc5682ed
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-03-21 09:43:34 -0400

    Continued AES implementation

--- src/common/crypto/aes.cpp
+++ src/common/crypto/aes.cpp
@@ -1,14 +1,18 @@
 #include "aes.hpp"
 #include <crypto++/aes.h>
 #include <crypto++/modes.h>
+#include <crypto++/filters.h>
 
 namespace crypto { namespace aes {
 
 bytes encrypt(bytes plaintext, key key) {
-    bytes iv(CryptoPP::AES::BLOCKSIZE, 0);
+    bytes ciphertext(plaintext.size() + 16, 0);
+    bytes iv(16, 0);
 
     CryptoPP::AES::Encryption aes_encryption(&key.front(), key.size());
-    //CryptoPP::CBC_Mode_ExternalCipher::Encryption cbc_encryption(aes_encryption, &iv.front(), iv.size());
+    CryptoPP::CBC_Mode_ExternalCipher::Encryption cbc_encryption(aes_encryption, &iv.front());
+
+    CryptoPP::StreamTransformationFilter filter(cbc_encryption, new CryptoPP::ArraySink(&ciphertext.front(), ciphertext.size()));
 }
 
 bytes decrypt(bytes ciphertex, key key) {
