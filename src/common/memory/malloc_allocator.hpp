f10cfdfca01e9764c544b828640d9d88a4d2fa08
Nathan Giraldeau <nathan.giraldeau@usherbrooke.ca>
2018-03-22 14:50:39 -0400

    Added simple memory allocators

---
+++ src/common/memory/malloc_allocator.hpp
@@ -0,0 +1,27 @@
+#ifndef MMAP_DEMO_HEAP_ALLOCATOR_HPP
+#define MMAP_DEMO_HEAP_ALLOCATOR_HPP
+
+#include "allocator_traits.hpp"
+
+#include <cstddef>
+
+namespace memory {
+
+class malloc_allocator {
+public:
+    raw_memory_ptr allocate(std::size_t size) const;
+
+    void free(raw_memory_ptr* memory, std::size_t size) const;
+};
+
+template<>
+struct allocator_traits<malloc_allocator> {
+    static const bool use_fixed_size_allocation = false;
+    static const bool can_allocate = true;
+    static const bool can_free = true;
+    static const bool can_clear = false;
+};
+
+}
+
+#endif //MMAP_DEMO_HEAP_ALLOCATOR_HPP
